bda51c8924a81e3d1e3743d34dca2755806e8cb6
fansty_like <fansty_like@hotmail.com>
2012-09-28 06:51:21 +0000

    新增ERP

---
+++ ERPSite/erp-dao/src/main/java/org/cloudland/dynamic/erp/dao/App.java
@@ -0,0 +1,13 @@
+package org.cloudland.dynamic.erp.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
