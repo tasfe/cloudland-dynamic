c769e27361ec66981c897032d934e182ca7cc755
fansty_like@hotmail.com <fansty_like@hotmail.com@be503435-582a-3b01-341f-39d7e31edf82>
2012-09-28 06:51:21 +0000

    新增ERP

---
+++ trunk/ERPSite/erp-core/src/main/java/org/cloudland/dynamic/erp/core/comons/number/UUID32.java
@@ -0,0 +1,33 @@
+/**
+  *
+  * @Title UUID32.java
+  * @Package org.cloudland.dynamic.core.commons.number
+  * @Description <p>TODO</p>
+  * @author Lei
+  * @date 2012-8-14
+  * @version 1.0
+  */
+package org.cloudland.dynamic.erp.core.comons.number;
+
+import java.util.UUID;
+
+/**
+ * 
+ * @ClassName UUID32
+ * @Description TODO
+ * @author Lei
+ * @date 2012-8-14 下午8:14:15
+ *
+ */
+public abstract class UUID32 {
+	
+	/**
+	  * 获取UUID长度32位号码
+	  * @Title get
+	  * @Description TODO
+	  * @return
+	  */
+	public static String get(){
+		return UUID.randomUUID().toString().replace("-", "").toUpperCase();
+	}
+}
